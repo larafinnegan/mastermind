edc1892cc50cad0430c0898810390e0aedf4d55f
Lara Finnegan <lcfinnegan@gmail.com>
2014-11-24 21:31:38 -0600

    few more methods

--- game.rb
+++ game.rb
@@ -9,20 +9,38 @@ end
 
 guesses = %w(red orange yellow green blue purple null white black)
 
-def generate_random_code
 code = Array.new(4)
 code.map! {|x| x = rand(6)+1}
 puts code
-end
 
-def get_guess
 puts "Please enter a 4 digit guess using numbers 1-6:"
     guess = gets.chomp.split("")
-    while code.length != 4
-    	puts "Please enter 4 numbers"
-	    guess = gets.chomp
-     end
+    while guess.length != 4
+        puts "Please enter 4 numbers"
+	    guess = gets.chomp.split("")
+    end
+    guess.map! (&:to_i)
+    
 
+    frequency = []
+    same = 0
+    present = 0
+    codecopy = code
+    for i in 0...guess.size
+        same +=1 if guess[i] == codecopy[i]
+    end
+    for i in 0...guess.length
+        if codecopy.include?(guess[i])
+            idx = codecopy.index(guess[i])
+            codecopy[idx] = nil
+            present += 1
+        end
+    end
+same.times {frequency << 9}
+(present-same).times {frequency << 8}
+
+
+frequency
 
 
 
