20b8db782c84123855e1e6c7f21dca4abf98d126
Lara Finnegan <lcfinnegan@gmail.com>
2014-11-26 19:53:48 -0600

    basic working program

--- board.rb
+++ board.rb
@@ -1,63 +1,56 @@
 class Board
-@@code = Array.new(4)
-def initialize
-	@board = []
-end
 
-def create_code
-code = Array.new(4)
-code.map! {|x| x = rand(6)+1}
-board << code
-end
+	def initialize
+		@board = []
+	end
 
-def feedback
-    codecopy = Array.new(board[0])
-    guess = board[-1]
-    frequency = []
-    same = 0
-    present = 0
-    for i in 0...guess.size
-        same +=1 if guess[i] == codecopy[i]
-    end
-    for i in 0...guess.length
-        if codecopy.include?(guess[i])
-            idx = codecopy.index(guess[i])
-            codecopy[idx] = nil
-            present += 1
-        end
-    end
-    same.times {frequency << 9}
-    (present-same).times {frequency << 8}
-    frequency
-end
+	def create_code
+		code = Array.new(4)
+		code.map! {|x| x = rand(6)+1}
+		@board << code
+	end
 
-def populate(input)
-board << input
-end
+	def feedback
+		codecopy = Array.new(@board[0])
+		guess = @board[-1]
+		frequency = []
+		same = 0
+		present = 0
+		for i in 0...guess.size
+			same +=1 if guess[i] == codecopy[i]
+		end
+		for i in 0...guess.length
+			if codecopy.include?(guess[i])
+				idx = codecopy.index(guess[i])
+				codecopy[idx] = nil
+				present += 1
+			end
+		end
+		same.times {frequency << 9}
+		(present-same).times {frequency << 8}
+		frequency
+	end
 
-def display_board
-for i in (1...(@@guesses*2)).step(2) do
-puts "guess #{((i/2)+1)}: #{board[i]}  feedback: #{board[i+1]}"
-end
-end
+	def populate(input)
+		@board << input
+	end
 
+	def display
+		puts @board[0]
+		for i in (1...@board.length).step(2)
+			puts "guess: #{@board[i]}  feedback: #{@board[i+1]}"
+		end
+	end
+	
+	def display_code
+		@board[0]
+	end
 
+	def incorrect_feedback(input)
+		input.all? { |x| (7..9).include?(x) } && input.count == 4
+	end
 
-
-
-def incorrect_feedback(input)
-	input.all? { |x| (7..9).include?(x) } && input.count == 4
-end
-
-
-def win?
-true if feedback.all? {|x| x == 9}
-end
-
-
-
-
-
-
-
+	def win?
+		true if @board[-2] == @board[0]
+	end
 end
--- game.rb
+++ game.rb
@@ -1,36 +1,54 @@
-require '.\player'
-require '.\peg'
 require '.\board'
 
 class Game
 
-def initialize
-end
+	@@guesses = 0
 
-def get_guess
-puts "Please enter a guess: "
-end
+	def initialize(board)
+		@board = board
+	end
 
-def validate_guess(guess = gets.chomp)
-    while guess.length != 4
-        puts "Please enter 4 numbers"
-        guess = gets.chomp
-    end 
-    guess
-end
+	def get_guess
+		puts "Please enter a guess: "
+	end
 
-def format_guess(guess)
-    guess.split("").map! (&:to_i)
-end
+	def validate_guess(guess = gets.chomp)
+		while guess.length != 4
+			puts "Please enter 4 numbers"
+			guess = gets.chomp
+		end 
+		guess
+	end
 
-def invalid_input?(input)
-	input.all? { |x| (1..6).include?(x) } && input.count == 4
-end
+	def format_guess(guess)
+		guess.split("").map! {|x| x.to_i}
+	end
 
+	def invalid_input?(input)
+		input.all? { |x| (1..6).include?(x) } && input.count == 4
+	end
+	
+	def increment_guesses
+		@@guesses += 1
+	end
+	
+	def play
+		@board.create_code
+		while @@guesses < 12 && !@board.win?
+			get_guess
+			@board.populate(format_guess(validate_guess))
+			@board.populate(@board.feedback)
+			@board.display
+			increment_guesses
+		end
+		if @board.win?
+			puts "Congrats, you guessed the code!!" 
+		else 
+			puts "You lose!  The code is: #{@board.display_code.join(" ")}"
+		end
+	end
 end
-game = Game.new
-board = Board.new
-board.create_code
-game.get_guess
-board.populate(game.format_guess(game.validate_guess))
-board.populate(board.feedback)
+game = Game.new(board = Board.new)
+game.play
+
+
